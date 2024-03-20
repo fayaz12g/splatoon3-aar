d9b58eed55afe051eaa458c1b8aee1e366cd483b
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-03-20 18:48:52 -0400

    suyu support

--- GUI.py
+++ GUI.py
@@ -86,6 +86,7 @@ corner_HUD = BooleanVar(value=True)
 
 # Generation
 output_yuzu = BooleanVar()
+output_suyu = BooleanVar()
 output_ryujinx = BooleanVar()
 open_when_done = BooleanVar()
 mod_name_var = StringVar(value="Fayaz's Settings")
@@ -283,6 +284,9 @@ def select_mario_folder():
     if output_ryujinx.get() is True:
         input_folder = f"C:/Users/{username}/AppData/Roaming/Ryujinx/mods/contents/0100B99019412000"
         process_name = "ryujinx.exe"
+    if output_suyu.get() is True:
+        input_folder = f"C:/Users/{username}/AppData/Roaming/suyu/load/0100B99019412000"
+        process_name = "suyu.exe"
     else:
         process_name = "yuzu.exe"
     if input_folder:
@@ -429,6 +433,7 @@ def pack_widgets():
     corner_checkbox.pack(padx=10, pady=10) 
 
     emulator_label.pack(pady=10)
+    suyu_checkbox.pack(side="top")
     yuzu_checkbox.pack(side="top")
     ryujinx_checkbox.pack(side="top")
 
@@ -490,6 +495,7 @@ def forget_packing():
 
     emulator_label.pack_forget()
     yuzu_checkbox.pack_forget()
+    suyu_checkbox.pack_forget()
     ryujinx_checkbox.pack_forget()
 
     output_folder_button.pack_forget()
@@ -685,6 +691,7 @@ notebook.add("Generate")
 emulator_label= customtkinter.CTkLabel(master=notebook.tab("Generate"), text="Select your Emulator OR choose a custom output folder, then click Generate.")
 yuzu_checkbox = customtkinter.CTkRadioButton(master=notebook.tab("Generate"), text="Yuzu", value=1, variable=output_yuzu, command=lambda: [output_ryujinx.set(False), repack_widgets])
 ryujinx_checkbox = customtkinter.CTkRadioButton(master=notebook.tab("Generate"), text="Ryujinx", value=2, variable=output_ryujinx, command=lambda: [output_yuzu.set(False), repack_widgets])   
+suyu_checkbox = customtkinter.CTkRadioButton(master=notebook.tab("Generate"), text="Suyu", value=2, variable=output_suyu, command=lambda: [output_yuzu.set(False), output_ryujinx.set(False), repack_widgets()])  
 
 output_folder_button = customtkinter.CTkButton(master=notebook.tab("Generate"), text="Custom Output Folder", fg_color="gray", hover_color="black", command=select_output_folder)
 
